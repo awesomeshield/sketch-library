a8dbe349c47f9e0394574ec6ade4d912a9b26212
Chris Palmer <cwjpalmer@gmail.com>
2017-04-11 15:19:34 +0200

    updated reaction timer functionality to match desired functionality

--- reaction-timer-using-setup-loop/reaction-timer-using-setup-loop.ino
+++ reaction-timer-using-setup-loop/reaction-timer-using-setup-loop.ino
@@ -9,37 +9,33 @@ boolean buttonWasPressed = false; // A variable to store if the button was press
 
 void setup() {
   awesome.setup(9600);
-
-  /*
-  The following code will run right after reset / first boot and only once:
-  It will wait a random numer of seconds (between 2 seconds and 5 seconds)
-  Record the number of seconds passed since last start/reset of arduino
-  and then turn on the LED.
+  /*The following code will run right after reset / first boot and only once:
+    It will wait a random numer of seconds (between 2 seconds and 5 seconds)
+    Record the number of seconds passed since last start/reset of arduino
+    and then turn on the LED.
   */
 
-  delay(random(2000,5000));
+  delay(random(2000, 5000));
   signalTime = millis();
-  awesome.LED.turnOn(RED);
+  awesome.LED.turnOn(GREEN);
 }
 
 void loop() {
-  /*  
-  Check if the button is down now or was down before
-  We do this because we only want to execute the code the first time the user 
-  presses the button
+  /*Check if the button is down now or was down before
+    We do this because we only want to execute the code the first time the user
+    presses the button
   */
-  if (awesome.button.isDown() && !buttonWasPressed) {
-    /*
-    Now record the button press so this code doesn't tun again,
-    compute the reaction time, print it to the computer and turn
-    the LED green so the user knows his button press was recorded.
+  if ( awesome.button.isDown() && !buttonWasPressed ) {
+    /*Now record the button press so this code doesn't run again,
+      compute the reaction time, print it to the computer and turn
+      the LED green so the user knows his button press was recorded.
     */
     buttonWasPressed = true;
     reactionTime = millis() - signalTime;
-    Serial.println(""); // Always insert a newline
+    Serial.println(""); // insert a newline at the beginning
     Serial.print("Your reaction time is ");
     Serial.print(reactionTime);
-    Serial.println(" ms"); // New line after last word   
-    awesome.LED.turnOn(GREEN);
+    Serial.println(" ms."); // New line after last word
+    awesome.LED.turnOff();
   }
 }
