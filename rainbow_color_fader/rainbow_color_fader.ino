61bd7712bc2e8e650b25cec5e30ed1306a7dd670
Chris Palmer <cwjpalmer@gmail.com>
2017-04-11 15:12:59 +0200

    set to fade or flash using toggleSwitch, improved comments

--- rainbow_color_fader/rainbow_color_fader.ino
+++ rainbow_color_fader/rainbow_color_fader.ino
@@ -3,59 +3,94 @@
 // tell the Arduino you're using the Awesome Shield hardware
 Awesome awesome;
 
-/*
- * Want to fade between the colors of the rainbow
- * Here are the colors, with RGB values
- * red      (255,0,0)
- * orange   (255,32,0)
- * yellow   (255,85,0)
- * green    (0,255,0)
- * blue     (0,0,255)
- * indigo   (0,150,255)
- * violet   (0,255,255)
- */
+/* RAINBOW MAKER!!!
 
-// declare variables here
-int delayTime=30;
+   This sketch shows the colors of the rainbow.
+   Here are the colors
+   with RGB values like this: (r, g, b)
+   red      (255,0,0)
+   orange   (255,32,0)
+   yellow   (255,85,0)
+   green    (0,255,0)
+   blue     (0,0,255)
+   indigo   (0,150,255)
+   violet   (0,255,255)
+
+   If the toggleSwitch is turned on, the LED flashes each color.
+   Otherwise it fades between the colors.
+*/
+
+
+// length of each pause while fading between colors
+int fadeDelay = 10;
+
+// length of each pause while flashing between colors
+int flashDelay = 500;
 
 void setup() {
   awesome.setup(9600);
-  // put your setup code here, to run once:
-  
+  // nothing else needs to be done in setup
+
 }
 
 void loop() {
-  // start on red
-  awesome.LED.turnOn(RED);
-  delay(1000);
-  
-  // red to yellow
-  for (int i = 0; i <= 85; i++ ) {
-    awesome.LED.turnOn(255,i,0);
-    delay(delayTime);
+  if ( awesome.toggleSwitch.isOn() ) {
+    // flash each color of the rainbow
+    awesome.LED.turnOn(RED);
+    delay(flashDelay);
+    awesome.LED.turnOn(ORANGE);
+    delay(flashDelay);
+    awesome.LED.turnOn(YELLOW);
+    delay(flashDelay);
+    awesome.LED.turnOn(GREEN);
+    delay(flashDelay);
+    awesome.LED.turnOn(BLUE);
+    delay(flashDelay);
+    // violet is similar to purple
+    awesome.LED.turnOn(PURPLE);
+    delay(flashDelay);
   }
-  
-  // yellow to green
-  for (int i = 0; i <= 255-85; i++ ) {
-    awesome.LED.turnOn(255-i,85+i,0);
-    delay(delayTime/2);
+  else {
+    // fade between the colors of the rainbow
+
+    // fade from red (255,0,0) to yellow (255,85,0) 
+    for (int i = 0; i <= 85; i++ ) {
+      /* at the beginning of the for loop, 
+         i = 0 so the LED.turnOn function gets
+         (255,0,0) as an input, which makes red.
+
+         i gradually increases until i = 85.
+         At this point the LED.turnOn function gets
+         (255,85,0) as an input, which makes yellow.
+      */
+      awesome.LED.turnOn(255, i, 0);
+      delay(fadeDelay);
+    }
+
+    // fade from yellow (255,85,0) to green (0,255,0)
+    for (int i = 0; i <= 255; i++ ) {
+      awesome.LED.turnOn(255 - i, 85 + i, 0);
+      delay(fadeDelay);
+    }
+
+    // fade from green (0,255,0) to blue (0,0,255)
+    for (int i = 0; i <= 255; i++ ) {
+      awesome.LED.turnOn(0, 255 - i, i);
+      delay(fadeDelay);
+    }
+
+    // fade from blue (0,0,255) to purple (0,255,255)
+    for (int i = 0; i <= 255; i++ ) {
+      awesome.LED.turnOn(i, 0, 255);
+      delay(fadeDelay);
+    }
+    // hold on purple for one second
+    delay(1000);
   }
 
-  // green to blue
-  for (int i = 0; i <= 255; i++ ) {
-    awesome.LED.turnOn(0,255-i,i);
-    delay(delayTime/3);
-  }
-
-  // blue to violet
-  for (int i = 0; i <= 255; i++ ) {
-    awesome.LED.turnOn(i,0,255);
-    delay(delayTime/3);
-  }
-  // hold on purple
-  delay(1000);
-  
-  // signal end of loop
+  /* turn off the LED and wait for one second
+     to signal the end of the loop
+  */
   awesome.LED.turnOff();
   delay(1000);
 }
